83875234d68c438c26729553917de8507ccb623b
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2023-12-03 23:04:16 -0300

    model User created and mirage configuration

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,22 @@
+import { createServer, Model, Server } from "miragejs";
+import { User } from "../../models/user";
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
+    routes() {
+      this.namespace = "api";
+      this.timing = 750;
+
+      this.get("/users");
+      this.post("users");
+
+      this.namespace = "";
+      this.passthrough();
+    },
+  });
+
+  return server;
+}
